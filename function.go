a590bad3715a4561a33d2accc3d9d32e247c4ff2
GalileoCap <galileocapp@gmail.com>
2023-05-26 15:32:43 -0300

    OK: Filtering, no depth

--- function.go
+++ function.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 type Function struct {
@@ -17,8 +17,68 @@ func (fun *Function) Call(args... *Variable) {
   fmt.Printf("Called: %v(%v) with %v\n", fun.Name, fun.Params, args)
 }
 
+func filterByName(options []*Function, name string) []*Function {
+  //fmt.Println("[filterByName]", name)
+  newOptions := make([]*Function, 0, len(options))
+
+  for _, fun := range options {
+    if fun.Name == name {
+      newOptions = append(newOptions, fun)
+    }
+  }
+
+  return newOptions
+}
+
+func filterByLen(options []*Function, l int) []*Function {
+  //fmt.Println("[filterByLen]", l)
+  newOptions := make([]*Function, 0, len(options))
+
+  for _, fun := range options {
+    if len(fun.Params) == l {
+      newOptions = append(newOptions, fun)
+    }
+  }
+
+  return newOptions
+}
+
+func filterByArg(options []*Function, p *Type, idx int) []*Function {
+  //fmt.Println("[filterByArg]", p, idx)
+  newOptions := make([]*Function, 0, len(options))
+
+  for _, fun := range options {
+    // TODO: Check it satisfies this param
+    newOptions = append(newOptions, fun)
+  }
+
+  return newOptions
+}
+
+func deepestOption(options []*Function) *Function {
+  if len(options) == 0 {
+    return nil
+  }
+
+  return options[0] // TODO: Actually deepest
+}
+
 func findFunction(name string, params []*Type) *Function {
-  return nil
+  options := Functions
+  //fmt.Println(options)
+
+  options = filterByName(options, name)
+  //fmt.Println(options)
+
+  options = filterByLen(options, len(params))
+  //fmt.Println(options)
+
+  for i, p := range params {
+    options = filterByArg(options, p, i)
+    //fmt.Println(options)
+  }
+
+  return deepestOption(options)
 }
 
 func DefineFunction(name string, params... *Type) bool {
@@ -36,7 +96,7 @@ func DefineFunction(name string, params... *Type) bool {
 }
 
 func CallFunction(name string, args... *Variable) {
-  types := make([]*Type, len(args))
+  types := make([]*Type, 0, len(args))
   for _, arg := range args {
     types = append(types, arg.Type)
   }
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+import (
+  "fmt"
+)
+
 func main() {
   if !DefineFunction("print", Generic) {
     panic("Couldn't define print for generic")
   }
 
   gen := NewVariable("gen", Generic)
+
+  fmt.Println()
   CallFunction("print", gen)
 }
